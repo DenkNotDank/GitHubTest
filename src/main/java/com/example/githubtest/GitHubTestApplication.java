844adc5b1faebb5a28e7fafea964878595c8f9df
DenkNotDank <77640625+DenkNotDank@users.noreply.github.com>
2022-09-27 11:30:53 -0400

    Added "goodbye world!" code in main method line 12

--- src/main/java/com/example/githubtest/GitHubTestApplication.java
+++ src/main/java/com/example/githubtest/GitHubTestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitHubTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GitHubTestApplication.class, args);
+		System.Out.Print("goodbye world!");
 	}
 
 }
